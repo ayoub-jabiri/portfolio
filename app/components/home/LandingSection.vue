021674c106ed29c9b700b2ab834b657515760cbf
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2025-12-08 22:48:59 +0100

    Fixed tailwind container issues

--- app/components/home/LandingSection.vue
+++ app/components/home/LandingSection.vue
@@ -1,17 +1,14 @@
 <template>
-  <section class="bg-[#2196F3] h-[calc(100vh-64px)] text-white">
-    <div class="container py-[150px]">
-      <div class="content grid grid-cols-12 items-start gap-5">
-        <div class="col-span-12 lg:col-span-6">
-          <h1 class="text-5xl font-bold mb-8">
-            Hello, I am <br />
-            Ayoub Jabiri a <br />
-            Front End Developer
-          </h1>
-          <a href="#" class="main-btn">Download CV</a>
+    <section class="bg-[#2196F3] h-[calc(100vh-64px)] text-white">
+        <div class="container h-full flex flex-col justify-center py-[150x]">
+            <div class="content">
+                <h1 class="text-5xl font-bold mb-8">
+                    Hello, I am <br />
+                    Ayoub Jabiri a <br />
+                    Front End Developer
+                </h1>
+                <a href="#" class="main-btn">Download CV</a>
+            </div>
         </div>
-        <div class="image col-span-12 lg:col-span-6">test</div>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
