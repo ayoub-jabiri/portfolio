efb87c3d7bb684e458b2d6b27953f1fee3517970
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-01-24 22:44:30 +0100

    Made enhancement in the portfolio section

--- app/components/portfolio/PortfolioProjects.vue
+++ app/components/portfolio/PortfolioProjects.vue
@@ -18,7 +18,7 @@
                 class="fixed left-0 top-0 w-screen h-screen z-10 p-6 bg-[#0000002b] flex justify-center items-center"
             >
                 <div
-                    class="relative bg-[#f8f8f8] p-6 max-md:pt-[45px] grid grid-cols-12 gap-5 max-md:w-full max-lg:w-[500px] lg:w-[700px] rounded-md"
+                    class="relative bg-[#f8f8f8] p-6 max-md:pt-[45px] grid grid-cols-12 items-center gap-5 max-md:w-full max-lg:w-[500px] lg:w-[700px] rounded-md"
                 >
                     <button
                         class="absolute top-2.5 right-2.5 text-3xl w-[30px] h-[30px] flex justify-center items-center cursor-pointer"
@@ -37,16 +37,21 @@
                         <h3 class="text-xl max-lg:text-center font-bold mb-2">
                             {{ project.title }}
                         </h3>
-                        <p class="max-md:text-center mb-4">
+                        <p class="max-lg:text-center mb-4">
                             {{ project.description }}
                         </p>
+                        <span
+                            class="block bg-[#e4e4e4] text-black text-sm text-center w-[60px] py-1 max-lg:mx-auto mb-4 rounded-md"
+                        >
+                            {{ project.category }}
+                        </span>
                         <div
                             class="flex max-lg:justify-center items-center gap-3"
                         >
                             <a
                                 :href="project.demo"
                                 target="_blank"
-                                class="bg-[#e4e4e4] text-[#777] hover:bg-[#9E9E9E] hover:text-white transition w-[130px] py-2 rounded-md text-center flex justify-center items-center gap-2"
+                                class="bg-transparent text-[#2196F3] hover:bg-[#2196F3] hover:text-white border border-[#2196F3] transition w-[130px] py-2 rounded-md text-center flex justify-center items-center gap-2"
                             >
                                 <i class="ri-slideshow-3-line"></i>
                                 <span>Live Demo</span>
@@ -55,7 +60,7 @@
                                 v-if="project.demo"
                                 :href="project.sourceCode"
                                 target="_blank"
-                                class="bg-[#e4e4e4] text-[#777] hover:bg-[#9E9E9E] hover:text-white transition w-[130px] py-2 rounded-md text-center flex justify-center items-center gap-2"
+                                class="bg-transparent text-[#2196F3] hover:bg-[#2196F3] hover:text-white border border-[#2196F3] transition w-[130px] py-2 rounded-md text-center flex justify-center items-center gap-2"
                             >
                                 <i class="ri-code-line"></i>
                                 <span>Source Code</span>
@@ -70,7 +75,7 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const projects = ref([
     {
@@ -80,6 +85,7 @@ const projects = ref([
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque doloremque odio ipsa eum, officia tenetur nostrum unde nesciunt culpa deleniti non necessitatibus! Recusandae quis aperiam asperiores! Nemo ab provident possimus?",
         demo: "https://ayoub-jabiri.github.io/static-template-8",
         sourceCode: "https://github.com/ayoub-jabiri/static-template-8.git",
+        category: "vue.js",
     },
     {
         cover: "/imgs/projects/project-1.webp",
@@ -88,6 +94,7 @@ const projects = ref([
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque doloremque odio ipsa eum, officia tenetur nostrum unde nesciunt culpa deleniti non necessitatibus! Recusandae quis aperiam asperiores! Nemo ab provident possimus?",
         demo: "https://ayoub-jabiri.github.io/static-template-8",
         sourceCode: "https://github.com/ayoub-jabiri/static-template-8.git",
+        category: "js",
     },
     {
         cover: "/imgs/projects/project-1.webp",
@@ -96,6 +103,7 @@ const projects = ref([
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque doloremque odio ipsa eum, officia tenetur nostrum unde nesciunt culpa deleniti non necessitatibus! Recusandae quis aperiam asperiores! Nemo ab provident possimus?",
         demo: "https://ayoub-jabiri.github.io/static-template-8",
         sourceCode: "https://github.com/ayoub-jabiri/static-template-8.git",
+        category: "nuxt.js",
     },
 ]);
 
@@ -106,7 +114,17 @@ const props = defineProps({
     },
 });
 
-console.log(props.currentCategory);
-
 const togglePopup = ref(false);
+
+const filtredProjects = computed(() => {
+    if (props.currentCategory.toLocaleLowerCase() == "all") {
+        return projects.value;
+    } else {
+        return projects.value.filter(
+            (project) =>
+                project.category.toLowerCase() ==
+                props.currentCategory.toLocaleLowerCase()
+        );
+    }
+});
 </script>
