9fb25122b765f79299cc16fbd6c5f6e2322d687d
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-02-03 15:40:42 +0100

    Added the project page content

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -1,5 +1,30 @@
 <template>
-    <section>
-        <h1>Hello World!</h1>
+    <section class="py-15" id="portfolio">
+        <div class="container">
+            <h1 class="text-3xl font-bold text-center mb-8">Projects</h1>
+            <FilterSection @sendCategory="updateCategory" />
+            <PortfolioProjects :currentCategory="currentCategory" />
+            <nuxt-link
+                to="/projects"
+                class="main-btn block text-[#2196f3] bg-transparent hover:bg-[#2196f3] hover:text-white border border-[#2196f3] p-3.5 mt-6 mx-auto main-transition"
+            >
+                View More
+            </nuxt-link>
+        </div>
     </section>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const title = "Portfolio",
+    description = "This is Portfolio Section";
+
+const currentCategory = ref("All");
+
+// Start Methods
+
+const updateCategory = (data) => {
+    currentCategory.value = data;
+};
+</script>
